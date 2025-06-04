88a5f3eb3e849a63af59eed97dd599cf6bf6d6cb
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2025-06-04 15:03:24 +0300

    Create setup_node_mydata.sh

---
+++ setup_node_mydata.sh
@@ -0,0 +1,33 @@
+#Setup docker, cli and compose    
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    DRY_RUN=1 sh ./get-docker.sh
+    sudo sh get-docker.sh
+    sudo apt -y install docker-compose
+    # for saving docker login to be able to push images
+    sudo apt -y install gnupg2 pass 
+    # change the storage folder for more space to commit the image
+    sudo docker rm -f $(docker ps -aq); docker rmi -f $(docker images -q)
+	sleep 5
+sudo systemctl stop docker
+sleep 5
+sudo umount /var/lib/docker
+sudo rm -rf /var/lib/docker
+sudo mkdir /var/lib/docker
+sudo mkdir -p /mnt/newdata/dev/mkdocker
+sudo mount --rbind /mnt/newdata/dev/mkdocker /var/lib/docker
+sudo systemctl start docker
+
+#Set a docker compose file    
+    mkdir microsuite
+    cd microsuite
+    git clone https://github.com/svassi04/MicroSuite.git
+    cd MicroSuite
+    
+    # Change to docker group
+    sudo newgrp docker
+    
+    # Run docker compose example
+    sudo docker compose up
+	
+	
+	
